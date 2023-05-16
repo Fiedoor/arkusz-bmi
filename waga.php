4a95d56f01437ba70c9cbe5840322cb64273e24b
Stanisław Fiedoruk <11120@kasprzak.edu.pl>
2023-05-16 09:37:50 +0200

    Niedokończony php i działający css

--- waga.php
+++ waga.php
@@ -23,27 +23,47 @@
         <div id="right">
             <h1>Podaj dane</h1>
             <form action="waga.php" method="post">
-                Waga: <input type="number">
-                Wzrost[cm]: <input type="number">
+                Waga: <input type="number" name="waga"> <br>
+                Wzrost[cm]: <input type="number" name="wzrost"> <br>
                 <input type="submit" value="Licz BMI i zapisz wynik">
                 <?php
-                // s1
+                // error_reporting(0);
+                $waga = $_POST['waga'];
+                $wzrost = $_POST['wzrost'];
+                $conn = mysqli_connect('localhost', 'root', "", 'egzamin');
+                if ($waga < 1 or $wzrost < 1) {
+
+                } else {
+                    $bmi = $waga / pow($wzrost, 2);
+                    $bmi = $bmi * 10000;
+                    echo "<br> Twoja waga: " . $waga . " Twój wzrost: " . $wzrost . "<br>BMI wynosi: " . $bmi;
+                    if ($bmi < 18) {
+                        $id = 1;
+                    } else if ($bmi > 19 and $bmi < 25) {
+                        $id = 2;
+                    } else if ($bmi > 26 and $bmi < 30) {
+                        $id = 3;
+                    } else {
+                        $id = 4;
+                    }
+                    mysqli_query($conn,"INSERT INTO wyniki (`bmi_id`,`data_pomiaru`,`wynik`) VALUES ('$id',"",'$bmi'");
+                }
                 ?>
             </form>
         </div>
-        <div id="bot">
-            <table>
-                <tr>
-                    <th>lp</th>
-                    <th>Interpretacja</th>
-                    <th>zaczyna się od..</th>
-                </tr>
-                <?php
-                //s2
-                ?>
-            </table>
-        </div>
     </main>
+    <div id="bot">
+        <table>
+            <tr>
+                <th>lp</th>
+                <th>Interpretacja</th>
+                <th>zaczyna się od..</th>
+            </tr>
+            <?php
+            //s2
+            ?>
+        </table>
+    </div>
     <footer>
         Autor: Stanisław Fiedoruk 4J
         <a href="kw2.jpg">Wynik działania kwerendy 2</a>
